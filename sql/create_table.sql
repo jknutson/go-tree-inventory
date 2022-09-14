e75b286e1dcee399ae802152003f236cb0ac5619
John Knutson <john.m.knutson@gmail.com>
2022-09-13 19:38:56 -0500

    update sql and add margin for alert (#4)

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -1,16 +1,30 @@
--- Table: public.tree_inventory_v1
+-- SEQUENCE: public.tree_inventory_v2_id_seq
 
--- DROP TABLE IF EXISTS public.tree_inventory_v1;
+-- DROP SEQUENCE IF EXISTS public.tree_inventory_v2_id_seq;
 
-CREATE TABLE IF NOT EXISTS public.tree_inventory_v1
+CREATE SEQUENCE IF NOT EXISTS public.tree_inventory_v2_id_seq
+    INCREMENT 1
+    START 1
+    MINVALUE 1
+    MAXVALUE 2147483647
+    CACHE 1
+    OWNED BY tree_inventory_v2.id;
+
+--- --- ---
+
+-- Table: public.tree_inventory_v2
+
+-- DROP TABLE IF EXISTS public.tree_inventory_v2;
+
+CREATE TABLE IF NOT EXISTS public.tree_inventory_v2
 (
-    id integer NOT NULL DEFAULT nextval('tree_inventory_v1_id_seq'::regclass),
+    id integer NOT NULL DEFAULT nextval('tree_inventory_v2_id_seq'::regclass),
     type character varying COLLATE pg_catalog."default",
-    location character varying COLLATE pg_catalog."default",
     notes text COLLATE pg_catalog."default",
     diameter_breast_height_inches numeric,
     diameter_dripline_feet numeric,
-    CONSTRAINT tree_inventory_v1_pkey PRIMARY KEY (id)
+    geom geometry,
+    CONSTRAINT tree_inventory_v2_pkey PRIMARY KEY (id)
 )
 WITH (
     OIDS = FALSE
